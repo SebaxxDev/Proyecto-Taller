237ca640ab29a9bf26bb25d79b371c57fda6e2a3
Luis <luis.olivares@alumnos.uach.cl>
2022-11-30 23:43:25 -0300

    avance4

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Index = () => {
   return (
     <div className="container">
-      
+
       <Head>
         <title>Fechas Importantes</title>
         <link rel="icon" href="/favicon.ico" />
@@ -20,20 +20,32 @@ const Index = () => {
         </ul>
       </nav>
       
-      <div className="barraSuperior">
-        Próximas actividades
-      </div>
 
       <main>
-        <Image
-          src="/images/reloj.jpg"
-          height={100} // Desired size with correct aspect ratio
-          width={200} // Desired size with correct aspect ratio
-        />
-
-        <div className="title">
-        wena
+        
+        <div className="barraSuperior">
+        Próximas actividades
         </div>
+        
+        <table>
+        <tr>
+            <td>BAIN081</td><td>Cálculo</td><td>Prueba</td><td>10/11/22</td>
+        </tr>
+        </table>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Código</th><th>Asignatura</th><th>Tipo</th><th>Fecha</th>
+            </tr>
+          </thead>
+          <tr>
+            <td>BAIN081</td><td>Cálculo</td><td>Prueba</td><td>10/11/22</td>
+          </tr>
+          <tr>
+            <td>BAIN075</td><td>Física</td><td>Prueba</td><td>18/12/22</td>
+          </tr>
+        </table>
       </main>
       
       <footer>
