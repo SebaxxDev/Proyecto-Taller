735e2f1809abc6868d64ba3fd021450c2f605baf
Eduardo Leal <edoleal67@gmail.com>
2022-12-28 10:52:57 -0300

    actualizado

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
-import React,{ useEffect, useState, useMemo} from "react"
+import React,{ useEffect, useState} from "react"
 import Head from "next/head"
 import Image from "next/image"
 import TextoReloj from "../components/textoreloj";
-
 import { LectorDatos } from "../components/lectorDatos";
-
 import CuentaRegresiva from "../components/cuentaRegresiva";
-
-
-
-
-
 
 const Index = ()=>{
     const [time, changeTime] = useState(new Date().toLocaleTimeString());
@@ -19,27 +12,46 @@ const Index = ()=>{
             changeTime(new Date().toLocaleTimeString());
         },1000);
     }, []);
+
     return(                
         <div className="container">
             <Head>
-                <title>Proyecto Taller</title>
-                <link rel="icon" href="/Uach_Logotipo.ico"/>
+                <title>Próximas Evaluaciones</title>
+                <link rel="icon" href="/images/Uach_Logotipo.png"/>
             </Head>
             <div className="logo">
-            <Image
-                src="/images/logo_uach.png"
-                height={111}
-                width={425}
-                alt="Logo"
+                <Image
+                    src="/images/logo_uach.png"
+                    width={2600}
+                    height={650}
+                    alt="Logo"
             /></div>
-            
+            <h1 className="titulo">Próximas Evaluaciones</h1>
             <div className="reloj">
-            <TextoReloj texto={time}/>
+                <TextoReloj texto={time}/>
             </div>
-            <h1 className="titulo">Próximas Actividades</h1>
-            <div className="cuentaRegresiva"><CuentaRegresiva/></div>
+            <div className="cajaRoja"><CuentaRegresiva/></div>
             <div className="container2"> {LectorDatos()}</div>
-    </div>     
+            <div className="linkinfoalumnos">
+                <a href="https://secure20.uach.cl/webInfoalumnos/login.aspx" target="_blank">
+                    <Image
+                        src="/images/infoalumnos.png"
+                        height={58}
+                        width={58}
+                        alt="Logo"
+                /></a>
+                    Infoalumnos
+            </div>
+            <div className="linksiveduc">
+                <a href="https://siveducmd.uach.cl/" target="_blank">
+                    <Image
+                        src="/images/infoalumnos.png"
+                        height={58}
+                        width={58}
+                        alt="Logo"
+                /></a>
+                Siveduc
+            </div>
+        </div>     
     );
 };export default Index;
-
---
+++ pages/indexoriginal.js
@@ -0,0 +1,45 @@
+import React,{ useEffect, useState, useMemo} from "react"
+import Head from "next/head"
+import Image from "next/image"
+import TextoReloj from "../components/textoreloj";
+
+import { LectorDatos } from "../components/lectorDatos";
+
+import CuentaRegresiva from "../components/cuentaRegresiva";
+
+
+
+
+
+
+const Index = ()=>{
+    const [time, changeTime] = useState(new Date().toLocaleTimeString());
+    useEffect(function(){
+        setInterval(()=>{
+            changeTime(new Date().toLocaleTimeString());
+        },1000);
+    }, []);
+    return(                
+        <div className="container">
+            <Head>
+                <title>Proyecto Taller</title>
+                <link rel="icon" href="/Uach_Logotipo.ico"/>
+            </Head>
+            <div className="logo">
+            <Image
+                src="/images/logo_uach.png"
+                height={111}
+                width={425}
+                alt="Logo"
+            /></div>
+            
+            <div className="reloj">
+            <TextoReloj texto={time}/>
+            </div>
+            <h1 className="titulo">Próximas Actividades</h1>
+            <div className="cuentaRegresiva"><CuentaRegresiva/></div>
+            <div className="container2"> {LectorDatos()}</div>
+    </div>     
+    );
+};export default Index;
+
