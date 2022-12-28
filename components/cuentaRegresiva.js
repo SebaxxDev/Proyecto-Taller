bd9e8b836cfa8820378e49c3d1dbf078660f4851
Lewis <luis.olivares@alumnos.uach.cl>
2022-12-27 22:21:03 -0300

    ordenado2

--- components/cuentaRegresiva.js
+++ components/cuentaRegresiva.js
@@ -1,18 +1,12 @@
 import { useState, useEffect } from "react";
 
 export default function CuentaRegresiva(){
-    
-    
     var fechaAct = fecha()//fecha local actual 
     var horaAct = hora() //hora local actual  
-    var primer_ramo = datos(fechaAct) //datos del ramo mas cercano actual
-    
+    var primer_ramo = datos(fechaAct) //datos del ramo mas cercano actual 
     var dias_faltantes = calculaDias(primer_ramo?.fecha,fechaAct) 
     var reloj_faltante = calculaReloj(primer_ramo?.hora, horaAct)
     
-
-   
-
     return(     
         <div className="tabla2">
             <table>
@@ -27,21 +21,17 @@ export default function CuentaRegresiva(){
             </table>
         </div> 
     )
-
 }
 
 export function datos(fechaD_M_A){
     const [datable, setDatable] = useState([])
-    
-    
     useEffect(()=>{
-        fetch("http://localhost:3000/data/asignaturas.json")
+        fetch("/data/asignaturas.json")
             .then(response => response.json())
             .then(datos => {
                 setDatable(datos)
             })
     }, []);
-
     datable.sort((a,b)=>{ 
         if(a.fecha[0] < b.fecha[0]){                    
             return -1;
@@ -49,8 +39,8 @@ export function datos(fechaD_M_A){
     });
     datable.sort((a,b)=>{ 
         if(a.fecha[1] < b.fecha[1]){
-                return -1;
-            }
+            return -1;
+        }
     });
     var cont=0
     var n=datable.length
@@ -62,10 +52,7 @@ export function datos(fechaD_M_A){
                     var aux1 = datable[j-1]
                     datable[j-1] = datable[j]
                     datable[j] = aux1
-                }  
-            }                   
-        }               
-    }
+    }}}}
     for(var k=1; k<cont;k++){
         var aux2 = datable[k]
         var l = k -1
@@ -80,9 +67,7 @@ export function datos(fechaD_M_A){
 
 export function hora(){
     var fechaLocal = new Date().toLocaleString().replace('-','/').split(',')[1].replace('-','/');
-    
     var fechaD_M_A = fechaLocal
-   
     return fechaD_M_A
 }
 
@@ -92,30 +77,21 @@ export function fecha(){
     return fechaAct
 }
 
-export function calculaDias(fecha_ramo,fechas){//pulir la funcion para el tema de los meses
-    //fecha ramo > fecha local
+export function calculaDias(fecha_ramo,fechas){
     var texto = ""
-    
     var dias = fecha_ramo?.[0]-fechas[0]
-    
-    
     if(dias == 1){
         texto = dias + " día" 
     }else{
         texto = dias + " días"
     }
-
     return(texto)
 }
 
 export function calculaReloj(reloj_ramo, reloj){
-    //reloj ramo: "17:30:00"
-    //reloj: 
-    
     var texto = ""
     var reloj_1 = reloj.split(":")
     var reloj_ram = reloj_ramo?.split(":")
-    
     var hrs =  reloj_ram?.[0] - reloj_1[0]
     var min = reloj_ram?.[1] - reloj_1[1]
     if(reloj_ram?.[2] == 0){
@@ -130,7 +106,6 @@ export function calculaReloj(reloj_ramo, reloj){
     if(hrs<0){
         hrs = 24+hrs
     }
-
     if(sgd < 10){
         sgd = "0"+sgd
     }
@@ -140,7 +115,6 @@ export function calculaReloj(reloj_ramo, reloj){
     if(hrs < 10){
         hrs = "0"+hrs
     }
-
     texto = hrs+":"+min+":"+sgd
     return texto
     
@@ -149,46 +123,35 @@ export function calculaReloj(reloj_ramo, reloj){
 
 export function fraseMotivacional(diasFalt){
     const [frases, setFrases] = useState([])
-
     useEffect(()=>{
-        fetch("http://localhost:3000/data/frases.json")
+        fetch("/data/frases.json")
             .then(response => response.json())
             .then(datos => {
                 setFrases(datos)
             })
     }, []);
-    //console.log("frases:", frases[0])
-    
-    
     var i = diasFalt.split(" ")
     i = diasFalt[0]
     
-
     if(i > 7){
-        //console.log("Tienes tiempo suficiente para sacarte una buena nota")
         return frases[0]?.frase
     }
     if(i<=7 && i>5){
-        //console.log("Empieza a estudiar desde ahora")
         return frases[1]?.frase
     }
     if(i<=5 && i>3){
         return frases[2]?.frase
     }
     if(i<=3 && i>2){
-        //console.log("queda poquito")
         return frases[3]?.frase
     }
     if(i<=2 && i>1){
         return frases[4]?.frase
     }
     if(i==1){
-        //console.log("la prueba es mañana!!")
         return frases[5]?.frase
     }
-    
     if(i == 0){
-        //console.log("Exito en tu prueba")
         return frases[6]?.frase
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
-import React,{ useEffect, useState, useMemo} from "react"
+import React,{ useEffect, useState} from "react"
 import Head from "next/head"
 import Image from "next/image"
 import TextoReloj from "../components/textoreloj";
-
 import { LectorDatos } from "../components/lectorDatos";
-
 import CuentaRegresiva from "../components/cuentaRegresiva";
 
 const Index = ()=>{
     const [time, changeTime] = useState(new Date().toLocaleTimeString());
-
     useEffect(function(){
         setInterval(()=>{
             changeTime(new Date().toLocaleTimeString());
         },1000);
     }, []);
-
 
     return(                
         <div className="container">
@@ -23,49 +19,39 @@ const Index = ()=>{
                 <title>Próximas Evaluaciones</title>
                 <link rel="icon" href="/images/Uach_Logotipo.png"/>
             </Head>
-
             <div className="logo">
-            <Image
-                src="/images/logo_uach.png"
-                width={524}
-                height={130}
-                alt="Logo"
-            />
-            </div>
-
+                <Image
+                    src="/images/logo_uach.png"
+                    width={524}
+                    height={130}
+                    alt="Logo"
+            /></div>
             <h1 className="titulo">Próximas Evaluaciones</h1>
-
             <div className="reloj">
                 <TextoReloj texto={time}/>
             </div>
-
             <div className="cajaRoja"><CuentaRegresiva/></div>
-            
             <div className="container2"> {LectorDatos()}</div>
-
             <div className="linkinfoalumnos">
                 <a href="https://secure20.uach.cl/webInfoalumnos/login.aspx" target="_blank">
-                <Image
-                src="/images/infoalumnos.png"
-                height={58}
-                width={58}
-                alt="Logo"
-            /></a>
-            Infoalumnos
+                    <Image
+                        src="/images/infoalumnos.png"
+                        height={58}
+                        width={58}
+                        alt="Logo"
+                /></a>
+                    Infoalumnos
             </div>
-
             <div className="linksiveduc">
                 <a href="https://siveducmd.uach.cl/" target="_blank">
-                <Image
-                src="/images/infoalumnos.png"
-                height={58}
-                width={58}
-                alt="Logo"
-            /></a>
-            Siveduc
+                    <Image
+                        src="/images/infoalumnos.png"
+                        height={58}
+                        width={58}
+                        alt="Logo"
+                /></a>
+                Siveduc
             </div>
-
         </div>     
     );
 };export default Index;
-
