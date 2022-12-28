d9dd0063b2109a344052d49d03bd8f1b629839b6
Eduardo Leal <edoleal67@gmail.com>
2022-12-27 22:18:06 -0300

    ordenado

--- components/lectorDatos.js
+++ components/lectorDatos.js
@@ -1,71 +1,41 @@
-import { useSafeLayoutEffect } from "@chakra-ui/react";
 import React,{ useEffect, useState, useMemo} from "react"
 import {
     Menu,
     MenuButton,
     MenuList,
-    MenuItem,
     MenuItemOption,
-    MenuGroup,
     MenuOptionGroup,
-    MenuDivider,
     Button,
     
 } from '@chakra-ui/react'
-import { bodyStreamToNodeStream } from "next/dist/server/body-streams";
-
-/*
-export async function hora(){
-    var fechaLocal = new Date().toLocaleString().replace('-','/').split(',')[1].replace('-','/');
-    
-    var fechaD_M_A = fechaLocal
-    console.log("fechaActual: ", fechaD_M_A)
-    return fechaD_M_A
-}*/
-
-
 
 export function LectorDatos(){
-    /*var fechaActual = fecha()
-    var horaActual = hora()*/
     var fechaLocal = new Date().toLocaleString().replace('-','/').split(',')[0].replace('-','/');
-    
     var fechaD_M_A = fechaLocal.split('/')
-
     const [datable, setDatable] = useState([])
-    /*
-    const btn = document.querySelector("button")
-    btn.addEventListener("click", function()){
-        
-    }*/
 
     useEffect(()=>{
-        fetch("http://localhost:3000/data/asignaturas.json")
+        fetch("/data/asignaturas.json")
             .then(response => response.json())
             .then(datos => {
                 setDatable(datos)
             })
     }, []);
 
-
-
     var [value, setValue] = useState()
     if(value == undefined){
         datable.sort((a,b)=>{ 
             if(a.fecha[2] < b.fecha[2]){                    
                 return -1;
-            }
-        });
+        }});
         datable.sort((a,b)=>{ 
             if(a.fecha[0] < b.fecha[0]){                    
                 return -1;
-            }
-        });
+        }});
         datable.sort((a,b)=>{ 
             if(a.fecha[1] < b.fecha[1]){
                 return -1;
-            }
-        });
+        }});
         var cont=0
         var n=datable.length
         for(var i=0; i<n; i++){
@@ -76,10 +46,7 @@ export function LectorDatos(){
                         var aux1 = datable[j-1]
                         datable[j-1] = datable[j]
                         datable[j] = aux1
-                    }  
-                }                   
-            }               
-        }
+        }}}}
         for(var k=1; k<cont;k++){
             var aux2 = datable[k]
             var l = k -1
@@ -88,11 +55,9 @@ export function LectorDatos(){
                 l--
         }
         datable[l+1] = aux2
-        }  
-    }
+        }}
     useEffect(()=>{     
         if(value==0 || value==undefined){
-            
             datable.sort((a,b)=>{ 
                 if(a.fecha[0] < b.fecha[0]){                   
                     return -1;
@@ -104,12 +69,10 @@ export function LectorDatos(){
             datable.sort((a,b)=>{ 
                 if(a.fecha[2] < b.fecha[2]){                    
                     return -1;
-                }
-            });
+            }});
             
         }
         if(value == 1){     
-               
             datable.sort((a,b)=>{ 
                 if(a.fecha[0] > b.fecha[0]){                 
                     return -1;
@@ -121,30 +84,28 @@ export function LectorDatos(){
             datable.sort((a,b)=>{ 
                 if(a.fecha[2] > b.fecha[2]){                    
                     return -1;
-                    }
-            });   
+            }});   
             
         }
         if(value == 2){
             datable.sort((a,b)=>{ 
                 if(a.codigo < b.codigo){
                     return -1;
-                }});
+            }});
         }
         if(value== 3){
             datable.sort((a,b)=>{ 
                 if(a.fecha[2] > b.fecha[2]){                    
                     return -1;
-                    }
-            });
+            }});
             datable.sort((a,b)=>{ 
                 if(a.fecha[0] < b.fecha[0]){                    
                     return -1;
-                }});
+            }});
             datable.sort((a,b)=>{ 
                 if(a.fecha[1] < b.fecha[1]){
                     return -1;
-                    }});
+            }});
             var cont=0
             var n=datable.length
             for(var i=0; i<n; i++){
@@ -155,24 +116,17 @@ export function LectorDatos(){
                             var aux1 = datable[j-1]
                             datable[j-1] = datable[j]
                             datable[j] = aux1
-                        }  
-                    }                   
-                }               
-            }
-        for(var k=1; k<cont;k++){
-            var aux2 = datable[k]
-            var l = k -1
+            }}}}
+            for(var k=1; k<cont;k++){
+                var aux2 = datable[k]
+                var l = k -1
             while(l>=0){
                 datable[l+1] = datable[l]
                 l--
             }
             datable[l+1] = aux2
-        }  
-        }
-    }, [value]);
-
+        }}}, [value]);
     return(
-        
         <>
         <div className="botonFiltro">
             <Menu>
@@ -204,8 +158,8 @@ export function LectorDatos(){
                 <tbody>
                     {datable.map(item => (
                         <tr key={item.id}>
-                            <td><button onClick={() => {botonsito(item.codigo)}}>{item.codigo}</button></td>
-                            <td><button onClick={() => {botonsito(item.codigo)}}>{item.asignatura}</button></td>
+                            <td>{item.codigo}</td>
+                            <td>{item.asignatura}</td>
                             <td>{item.tipo}</td>
                             <td>{item.fecha[0]}/{item.fecha[1]}/{item.fecha[2]}; {item.hora}</td>        
                         </tr>
@@ -214,17 +168,6 @@ export function LectorDatos(){
             </table>
         </div>
         </div>
-                    
-        
-        
         </>
     )
 }
-
-function botonsito(item) {
-   
-    console.log("item: ",item)
-    
-   
-    
-  }
